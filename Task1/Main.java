cd8f22d0901e68c1ac1fdc1b15111fe29f9a74e4
timurchik <timurckaaslanov@yandex.ru>
2023-07-22 13:11:05 +0300

    Added task1

---
+++ Task1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat one = new Cat();
+        one.setName("Тишка");
+        one.setAge(2);
+        one.greet();
+    }
+}
