faa0aaba5e1701f9d9ad472f2f3619d34ed7e6a3
timurchik <timurckaaslanov@yandex.ru>
2023-07-26 04:35:23 +0300

    Update methods update, Add, Delete. Added private count, isOpen

--- HomeWork2/Market.java
+++ HomeWork2/Market.java
@@ -1,6 +1,6 @@
 public class Market implements QueueBehaviour, MarketBehaviour{
-    public int count = 0;
-
+    private int count = 0;
+    private boolean isOpen = false;
     @Override
     public void countPeople() {
         System.out.printf("В очереди %d людей\n", count);
@@ -9,15 +9,17 @@ public class Market implements QueueBehaviour, MarketBehaviour{
     @Override
     public void Add() {
         count = count + 1;
-        System.out.println("В очередь добавился один человек");;
+        System.out.println("В очередь добавился один человек");
+        updateShopState();
     }
 
     @Override
     public void Delete() {
         count = count - 1;
-        System.out.println("Очередь покинул 1 человек");;
+        System.out.println("Очередь покинул 1 человек");
+        updateShopState();
     }
-    public void Update(){
+    public void updateShopState(){
         if (count == 0){
             countPeople();
             CloseShop();
